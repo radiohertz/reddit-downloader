61f9bb79661a0122533a7b183f5c5f4b1f69c4fe
David John <davidkjohn01@gmail.com>
2020-05-20 13:57:17 +0530

    updated upper limit

--- cli/cli.go
+++ cli/cli.go
@@ -56,8 +56,8 @@ func (r *Cli) Init(args []string) {
 	if len(values) > 1 {
 		l, _ := strconv.ParseInt(values[1].Value, 10, 32)
 
-		if l < 0 || l > 50 {
-			log.Println("Limit cannot be less than 0 or greater than 50.Defaulting to 25")
+		if l < 0 || l > 100 {
+			log.Println("Limit cannot be less than 0 or greater than 100.Defaulting to 25")
 			limit = LIMIT
 		} else {
 			limit = int(l)
